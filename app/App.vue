1eaa2aaebec9af1626b050d53ad7753b63286239
Hiroyuki Tanjo <expheno@gmail.com>
2017-03-30 12:24:12 +0900

    Create Vue.js + webpack project

---
+++ app/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./images/logo.png">
+    <p>{{message}}</p>
+    <input type="text" v-model="message">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'Hello, Vue.js!'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  margin: 50px;
+  text-align: center;
+  font-family: sans-serif;
+  p {
+    font-weight: bold;
+  }
+}
+</style>
---
+++ webpack.config.babel.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextWebpackPlugin from 'extract-text-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+export default {
+  entry: './app/main.js',
+  output: {
+    filename: IS_PROD ? '[name].[chunkhash:8].js' : '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: IS_PROD ?
+              ExtractTextWebpackPlugin.extract(['css-loader', 'sass-loader']) :
+              ['vue-style-loader', 'css-loader', 'sass-loader']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: IS_PROD ? '[name].[hash:8].[ext]' : '[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+    }),
+    new ExtractTextWebpackPlugin('[name].[hash:8].css'),
+    new CleanWebpackPlugin(['dist'])
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),
+    stats: 'minimal'
+  }
+};
